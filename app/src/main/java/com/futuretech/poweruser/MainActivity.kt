d8ce6afd34844d7ba3756143a90b388fcb592311
irewon1-lgtm <ire.won1@gmail.com>
2026-09-15 22:34:26 +0900

    fix: use ColumnScope weight without internal import

--- app/src/main/java/com/futuretech/poweruser/MainActivity.kt
+++ app/src/main/java/com/futuretech/poweruser/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
