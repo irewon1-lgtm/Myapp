7768133bfafb772dc45372d0f1e27444d17bb7ed
irewon1-lgtm <ire.won1@gmail.com>
2026-09-16 00:36:05 +0900

    chore: bump learning UI release to v1.2.7 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.futuretech.poweruser"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.2.6"
+        versionCode = 10
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
