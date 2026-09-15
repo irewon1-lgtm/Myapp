de2d75d27ec9f1f52dee67aa3f1b668b2182624e
irewon1-lgtm <ire.won1@gmail.com>
2026-09-16 00:51:11 +0900

    chore: bump Extreme 26-30 delivery to v1.2.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.futuretech.poweruser"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.2.7"
+        versionCode = 11
+        versionName = "1.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
