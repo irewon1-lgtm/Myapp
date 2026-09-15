a5e65805b1f57b1215f64d5f96355e35e440258e
irewon1-lgtm <ire.won1@gmail.com>
2026-09-15 14:26:38 +0900

    Fix Compose UI assertion imports in review instrumentation

--- app/src/androidTest/java/com/futuretech/poweruser/education/ReviewUiInstrumentedTest.kt
+++ app/src/androidTest/java/com/futuretech/poweruser/education/ReviewUiInstrumentedTest.kt
@@ -1,7 +1,5 @@
 package com.futuretech.poweruser.education
 
-import androidx.compose.ui.test.assertDoesNotExist
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
