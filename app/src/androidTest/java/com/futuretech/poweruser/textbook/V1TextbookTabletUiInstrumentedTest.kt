63514935ac73fca8243adba6f25bed9fdb779818
irewon1-lgtm <ire.won1@gmail.com>
2026-09-16 19:22:02 +0900

    Verify centered ebook reader on Galaxy Tab layout

--- app/src/androidTest/java/com/futuretech/poweruser/textbook/V1TextbookTabletUiInstrumentedTest.kt
+++ app/src/androidTest/java/com/futuretech/poweruser/textbook/V1TextbookTabletUiInstrumentedTest.kt
@@ -29,7 +29,7 @@ class V1TextbookTabletUiInstrumentedTest {
     }
 
     @Test
-    fun galaxyTabSizedWindowUsesFocusedTrackLessonReader() {
+    fun galaxyTabSizedWindowUsesCenteredFixedPageBookReader() {
         val instrumentation = InstrumentationRegistry.getInstrumentation()
         val context = instrumentation.targetContext
         val config = context.resources.configuration
@@ -50,18 +50,18 @@ class V1TextbookTabletUiInstrumentedTest {
             return
         }
 
-        assertTagPresent("curriculum_overview_root")
-        assertTagPresent("curriculum_chapter_V1-C01")
-        compose.onNodeWithTag("curriculum_chapter_V1-C01").performClick()
-        assertTagPresent("v1_textbook_root")
-        assertTagPresent("textbook_section_strip")
-        assertTagPresent("textbook_reader")
-        assertTagPresent("textbook_section_progress")
-        assertTagAbsent("textbook_concept_progress")
-        assertTagAbsent("textbook_toc")
+        assertTagPresent("learning_home")
+        assertTagPresent("home_book_cover")
+        compose.onNodeWithTag("home_continue").performClick()
+        assertTagPresent("ebook_textbook_root")
+        assertTagPresent("reader_single_page_shell")
+        assertTagPresent("ebook_reader")
+        assertTagPresent("ebook_page_counter")
+        assertTagPresent("ebook_track_cover")
+        assertTagAbsent("textbook_section_strip")
         assertTagAbsent("textbook_insight_rail")
 
-        Log.i(EVIDENCE_TAG, "READY_FOR_SCREENSHOT widthDp=$widthDp heightDp=$heightDp trackLessonReader=true")
+        Log.i(EVIDENCE_TAG, "READY_FOR_SCREENSHOT widthDp=$widthDp heightDp=$heightDp fixedPageBookReader=true")
         Thread.sleep(15_000)
         Log.i(EVIDENCE_TAG, "SCREENSHOT_WINDOW_COMPLETE")
     }
@@ -77,6 +77,6 @@ class V1TextbookTabletUiInstrumentedTest {
     }
 
     companion object {
-        const val EVIDENCE_TAG = "V2TrackTabletEvidence"
+        const val EVIDENCE_TAG = "V3EbookTabletEvidence"
     }
 }
