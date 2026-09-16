c723a300bf95a693686875f634897777b77c5052
irewon1-lgtm <ire.won1@gmail.com>
2026-09-16 23:52:16 +0900

    Bump app release to 1.2.14 for verified textbook update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.futuretech.poweruser"
         minSdk = 26
         targetSdk = 34
-        versionCode = 16
-        versionName = "1.2.13"
+        versionCode = 17
+        versionName = "1.2.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
