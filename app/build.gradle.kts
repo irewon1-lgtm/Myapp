577e7801d3c11c34e71dba666d28d4cac19dfdca
irewon1-lgtm <ire.won1@gmail.com>
2026-09-16 19:52:44 +0900

    Bump ebook reader release to v1.2.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.futuretech.poweruser"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.2.12"
+        versionCode = 16
+        versionName = "1.2.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
