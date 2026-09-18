ce43d03cea1d3ec1431a905a32b896bc486de2b9
irewon1-lgtm <ire.won1@gmail.com>
2026-09-18 15:56:11 +0900

    release: publish textbook live v1.2.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.futuretech.poweruser"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.2.16"
+        versionCode = 20
+        versionName = "1.2.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
