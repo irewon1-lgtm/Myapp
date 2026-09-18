5aafd0c4bba90040ed57a1922cf4aa438f95de39
irewon1-lgtm <ire.won1@gmail.com>
2026-09-18 22:50:28 +0900

    prepare loading-screen fix v1.2.21 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.futuretech.poweruser"
         minSdk = 26
         targetSdk = 34
-        versionCode = 23
-        versionName = "1.2.20"
+        versionCode = 24
+        versionName = "1.2.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
